a1dbba7638efcaf32d3b82bbea92cf5bbc0d2b19
ochiecommerce <ochieaustine98@gmail.com>
2025-02-21 15:57:46 +0300

    Add files via upload

---
+++ precache-manifest.7a3676f4781c30dd817dd3d9e09a270e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39767b8d721cd2ef865219c678842390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e70412628ded10ec50",
+    "url": "/static/css/main.18924e37.chunk.css"
+  },
+  {
+    "revision": "d83636c115bd2746ffcf",
+    "url": "/static/js/2.bf6becff.chunk.js"
+  },
+  {
+    "revision": "106a59e860fbcbcc7a32f2d5cc56e2c3",
+    "url": "/static/js/2.bf6becff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb51ffc5345d6a5d026",
+    "url": "/static/js/3.1e3c1d58.chunk.js"
+  },
+  {
+    "revision": "76e70412628ded10ec50",
+    "url": "/static/js/main.cccf568a.chunk.js"
+  },
+  {
+    "revision": "4db9c4701802cce1c61e",
+    "url": "/static/js/runtime-main.7732e457.js"
+  }
+]);
